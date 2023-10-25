80d6cbbaa5e0b2eeb46ddb3ad6574e8d3fe1d8f5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-25 23:22:09 +0600

    added function more exaple

--- arroFunction.ts
+++ arroFunction.ts
@@ -4,3 +4,13 @@ const person = (s: string): string => {
   return "My Friend name is Mehedi and Emon";
 };
 console.log(person("HU"));
+
+/* make a function and use array and good practices */
+let heros = ["IronMan", "Thor", "SpiderMan", "BatMan"];
+const Hero = (yes: string) => {
+  heros.map((hero): string => {
+    return `I like the most ${hero}`;
+  });
+};
+
+console.log(Hero("yes"));
