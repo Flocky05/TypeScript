f068e22735b4a71e8eeb5a73568aeabb7876b8a0
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-25 21:41:06 +0600

    added variable method

--- Day1/variable.ts
+++ Day1/variable.ts
@@ -1,1 +1,9 @@
-console.log("Who are you?");
+/* At frist any variable decleared as js but after declearing you must be use clone(:) and type and the syntax will be:
+let variableName:type=value; 
+*/
+let nameing: string = "rakibul";
+
+let number: number = 54;
+let newnameing = nameing.toUpperCase();
+console.log(nameing, number);
+console.log(newnameing);
