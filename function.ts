e2d68671f57b00416a739a0421bd0556c59ae2d0
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-25 22:40:29 +0600

    make a sentence uppar case

--- function.ts
+++ function.ts
@@ -1,4 +1,13 @@
+/* you have to declear the num insite the parameter  */
 function addtwoNumber(num1: number, num2: number) {
+  if (num1 > 5) return num1 + 10;
   return num1 + num2;
 }
-console.log(addtwoNumber(2, 3));
+console.log(addtwoNumber(1, 3));
+
+/* write a function that return all the string lowerCase to UpperCase */
+
+function toUpper(val: string) {
+  return val.toUpperCase();
+}
+console.log(toUpper("My name is Rakibul Islam. what is your name?"));
