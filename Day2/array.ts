800462b98ce68c214cd20eceb1383133c290e070
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 13:27:00 +0600

    added array

---
+++ Day2/array.ts
@@ -0,0 +1,6 @@
+const superHeros: string[] = ["Locky", "Thor"];
+const superPowers: number[] = [10, 45, 56, 98, 65];
+superHeros.push("SpiderMan");
+superPowers.push(12);
+
+console.log(superHeros, superPowers);
