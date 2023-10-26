4592cb078f95314f55b0d5f5905da4f3ba914841
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 11:39:51 +0600

    update object

--- Day2/myObject.ts
+++ Day2/myObject.ts
@@ -1,5 +1,5 @@
 /* This way may use in js but we don't use like this way in typeScript */
-const Person = {
+/* const Person = {
   name: "Rakibul",
   mail: "md.rakibul.islam.0501@gmail.com",
   age: 23,
@@ -12,9 +12,36 @@ function creatUser(name: string, email: string) {
 }
 
 console.log(creatUser("rakibul", "what the hell"));
-
+ */
 /* CreateCourse a function */
 
 function createCourse(): { name: string; isPaid: boolean } {
   return { name: "Reactjs", isPaid: true };
 }
+
+/* Now start with object that is used in typeScript this is most important like below */
+
+type User = {
+  name: string;
+  email: string;
+  age: number;
+  isStudent: false;
+};
+
+function createUser(user: User): User {
+  return {
+    name: "rakibul",
+    email: "rakibul@gmail.com",
+    age: 230,
+    isStudent: false,
+  };
+}
+
+console.log(
+  createUser({
+    name: "rakibul",
+    email: "rakibul@gmail.com",
+    age: 23,
+    isStudent: false,
+  })
+);
