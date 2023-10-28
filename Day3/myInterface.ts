bbf749b571495529607a060bc45e6d28fed19b76
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-28 23:05:05 +0600

    addeed reopen interface

--- Day3/myInterface.ts
+++ Day3/myInterface.ts
@@ -5,11 +5,16 @@ interface User {
   id: number;
   googleId?: number;
 }
+/* Some times you have to need added a new element in site the interface but you can't do it, thats why you have to carete a new interface but it also can't do you, so you have to open thus interface where you added the value, its very very simple , just call the interface and use the value */
+interface User {
+  github: string;
+}
 const person: User = {
   dbid: 22217,
   user: "rakibul",
   email: "rakibul@gmail.com",
   id: 2527,
+  github: "flocky05",
 };
 person.email = "amirtumi@gmail.com";
 console.log(person);
